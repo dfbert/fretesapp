c380d67f1f49f5466f8efcf8bf42f9430b31bcc0
dfbert <dfbert@live.com>
2016-04-11 22:46:04 -0300

    aaa

--- scripts/GLOBAL.js
+++ scripts/GLOBAL.js
@@ -1,3 +1,7 @@
 var config__apisite = "http://fretesweb.com.br/app_api/";
 var config__maximgsize = 1.5; //mb
 
+function clearAll(){
+	localStorage.clear();
+     localStorage.setItem("welcome", true);
+}
