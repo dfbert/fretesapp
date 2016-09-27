18fdf8a94ce742ee8f5e23c2b448377b86a4e1bc
dfbert <dfbert@live.com>
2016-09-27 09:29:35 -0300

    sdsds

--- scripts/GLOBAL.js
+++ scripts/GLOBAL.js
@@ -13,7 +13,7 @@ var pushNotification;
 			
 				try 
 				{ 
-                	pushNotification = window.plugins.pushNotification;
+                	pushNotification = window.plugins.PushNotification;
                 	if (device.platform == 'android' || device.platform == 'Android' ||
                             device.platform == 'amazon-fireos' ) {
 			pushNotification.register(successHandler, errorHandler, {"senderID":"225827957905","ecb":"onNotification"});		// required!
