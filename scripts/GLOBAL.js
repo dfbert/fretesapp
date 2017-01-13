3d7a0b976807f5899e802450c1372a8320147322
dfbert <dfbert@live.com>
2017-01-13 00:34:33 -0200

    fgfgfgfgfgg

--- scripts/GLOBAL.js
+++ scripts/GLOBAL.js
@@ -5,40 +5,10 @@ function clearAll(){
 	localStorage.clear();
      localStorage.setItem("welcome", true);
 }
-function initPushwoosh() {
-  var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
-
-  // Should be called before pushwoosh.onDeviceReady
-  document.addEventListener('push-notification', function(event) {
-    var notification = event.notification;
-    // handle push open here
-  });
-  
-  // Initialize Pushwoosh. This will trigger all pending push notifications on start.
-  pushwoosh.onDeviceReady({
-    appid: "FB3FB-27698",
-    projectid: "225827957905",
-    serviceName: "MPNS_SERVICE_NAME"
-  });
-
-pushwoosh.registerDevice(
-  function(status) {
-    var pushToken = status.pushToken;
-     alert(pushToken);
-  },
-  function(status) {
-    // handle registration error here
-  }
-);
-
-
-}
 
 
             function ondeviceready() {   
    alert('Device is ready!');
-   initPushwoosh();
-}
             
             
             
